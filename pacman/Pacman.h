20e3b4f837f51ae2b40cfb1d9b9de6f3364c554e
linsinrou <101238809+linsinrou@users.noreply.github.com>
2022-06-08 00:35:18 +0800

    Add files via upload
    自動追蹤P

--- pacman/Pacman.h
+++ pacman/Pacman.h
@@ -5,7 +5,14 @@ class Pacman {
 public:
 	Pacman();
 	Pacman(int , int);
+/*
 	void ShowMap();
+	void movex(int, int, int);
+	void movey(int, int, int);
+	void move(int, int, int, int);
+	int getx(int);
+	int gety(int);
+*/
 
 private:
 	int x;
